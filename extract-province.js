3a4b5c522a00086c07882e97461894caa0864f6c
Nate Bayles <nbayles@hotmail.com>
2019-02-01 23:38:43 -0700

    Update extract-province.js

--- extract-province.js
+++ extract-province.js
@@ -5,7 +5,26 @@ $('#province option').each(function() {
 
 var i = arr[1];
 	$.post("zipcode-municipality.php", {id:i}, function(data) {
-    		var rex = RegExp("<\s*li[^>]*>(.*?)<\s*/\s*li>", "g");
-    		var rexResult = rex.exec(data); 
-		console.log(rexResult[0]);
-	});
+	var rex = RegExp("<\s*li[^>]*>(.*?)<\s*/\s*li>", "g");
+	var rexResult = rex.exec(data);
+	
+	var info = {
+		postalCode: extractPostalCode(rexResult[0]),
+		city: extractCity(rexResult[0]),
+		province: extractProvince(rexResult[0])
+	};
+	
+	console.log(info);
+});
+
+function extractPostalCode(input) {
+	return input.substring(input.indexOf("<strong>")+8, input.indexOf("</strong>"));
+}
+
+function extractCity(input) {
+	return input.substring(input.indexOf(" - ")+3, input.indexOf(" ,"));
+}
+
+function extractProvince(input) {
+	input.substring(input.indexOf(" ,")+2, input.indexOf("</li>"));
+}
