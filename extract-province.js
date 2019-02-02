e99a29c7fc702fb9ac5a753b8fa6ba701e505605
Nate Bayles <nbayles@hotmail.com>
2019-02-01 23:50:43 -0700

    Update extract-province.js

--- extract-province.js
+++ extract-province.js
@@ -1,42 +1,40 @@
 var provinces = [];
-$('#province option').each(function() {
-	provinces.push($(this).val());
+$('#province option').each(function () {
+    provinces.push($(this).val());
 });
 
-var infos = [];
+
 var i = provinces[13];
-	$.post("zipcode-municipality.php", {id:i}, function(data) {
-	var rex = RegExp("<\s*li[^>]*>(.*?)<\s*/\s*li>", "g");
-		
-		
-	var rexResult;
-	do {
-    		rexResult = rex.exec(data);
-    		if (rexResult) {
-        		var info = {
-				postalCode: extractPostalCode(rexResult[0]),
-				city: extractCity(rexResult[0]),
-				province: extractProvince(rexResult[0])
-			};
-			infos.push(info);
-    		}
-	} while (rexResult);	
-	
-	
+$.post("zipcode-municipality.php", { id: i }, function (data) {
+    var rex = RegExp("<\s*li[^>]*>(.*?)<\s*/\s*li>", "g");
+
+    var infos = [];
+    var rexResult;
+    do {
+        rexResult = rex.exec(data);
+        if (rexResult) {
+            var info = {
+                postalCode: extractPostalCode(rexResult[0]),
+                city: extractCity(rexResult[0]),
+                province: extractProvince(rexResult[0])
+            };
+            infos.push(info);
+        }
+    } while (rexResult);
 });
 
 infos.forEach(info => {
-	console.log('"'+info.postalCode+'","'+info.city+'","'+info.province+'"');
+    console.log('"' + info.postalCode + '","' + info.city + '","' + info.province + '"');
 });
 
 function extractPostalCode(input) {
-	return input.substring(input.indexOf("<strong>")+8, input.indexOf("</strong>"));
+    return input.substring(input.indexOf("<strong>") + 8, input.indexOf("</strong>"));
 }
 
 function extractCity(input) {
-	return input.substring(input.indexOf(" - ")+3, input.indexOf(" ,"));
+    return input.substring(input.indexOf(" - ") + 3, input.indexOf(" ,"));
 }
 
 function extractProvince(input) {
-	return input.substring(input.indexOf(" ,")+2, input.indexOf("</li>"));
+    return input.substring(input.indexOf(" ,") + 2, input.indexOf("</li>"));
 }
