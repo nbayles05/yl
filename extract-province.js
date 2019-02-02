7e59394096ec1b2206275aa74fed9f50cc897e96
Nate Bayles <nbayles@hotmail.com>
2019-02-01 23:30:31 -0700

    Update extract-province.js

--- extract-province.js
+++ extract-province.js
@@ -2,22 +2,10 @@ var arr = [];
 $('#province option').each(function() {
 	arr.push($(this).val());
 });
-//console.log(arr);
 
-//arr.forEach(i => {
 var i = arr[1];
 	$.post("zipcode-municipality.php", {id:i}, function(data) {
-		//console.log(data);
     		var rex = RegExp("<\s*li[^>]*>(.*?)<\s*/\s*li>", "g");
     		var rexResult = rex.exec(data); 
 		console.log(rexResult);
-		//var str = rexResult[1];
-		//var id = str.substring(str.indexOf("<strong>")+8, str.indexOf("</strong>"));
-		//var city = str.substring(str.indexOf(" - ")+3, str.indexOf(" ,"));
-		//var province = str.substring(str.indexOf(" ,")+2, str.indexOf("</li>"));
-		//console.log(id,city,province);
 	});
-//});
-
-//var rex = RegExp("<\s*li[^>]*>(.*?)<\s*/\s*li>", "g");
-//console.log(rex.match(content))
